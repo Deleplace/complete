8ad7461381bc37e4367c2bf914cd33e84be6d8d5
Deleplace <deleplace2015@gmail.com>
2017-07-21 19:11:38 +0200

    Draft: explore GOPATH folders for directory/package names.

--- gocomplete/pkgs.go
+++ gocomplete/pkgs.go
@@ -3,7 +3,11 @@ package main
 import (
 	"go/build"
 	"io/ioutil"
+	"os"
+	"os/user"
 	"path/filepath"
+
+	"strings"
 
 	"github.com/posener/complete"
 )
@@ -11,11 +15,59 @@ import (
 // predictPackages completes packages in the directory pointed by a.Last
 // and packages that are one level below that package.
 func predictPackages(a complete.Args) (prediction []string) {
-	prediction = complete.PredictFilesSet(listPackages(a.Directory())).Predict(a)
-	if len(prediction) != 1 {
-		return
+	// prediction = complete.PredictFilesSet(listPackages(a.Directory())).Predict(a)
+	// if len(prediction) != 1 {
+	// 	return
+	// }
+	// return complete.PredictFilesSet(listPackages(prediction[0])).Predict(a)
+
+	gopathEntries := findGopath()
+	// complete.Log("gopathEntries = %v", gopathEntries)
+	ossep := string([]byte{os.PathSeparator})
+	fragment := strings.Replace(a.Last, "/", ossep, -1)
+	for _, gopath := range gopathEntries {
+		gosrc := filepath.Join(gopath, "src")
+		fragmentPath := filepath.Join(gosrc, fragment)
+		// complete.Log("fragmentPath is %v", fragmentPath)
+		dir := filepath.Dir(fragmentPath)
+		// complete.Log("dir is %v", dir)
+		infos, err := ioutil.ReadDir(dir)
+		if err != nil {
+			complete.Log("listing directory %s: %s", dir, err)
+			continue
+		}
+		for _, info := range infos {
+			// complete.Log("info.Name() is %v", info.Name())
+			candidatePath := filepath.Join(dir, info.Name())
+			if info.IsDir() && strings.HasPrefix(candidatePath, fragmentPath) {
+				completionItem := candidatePath[len(gosrc):]
+				completionItem = strings.Trim(completionItem, "/\\")
+				if !strings.HasPrefix(completionItem, fragment) {
+					complete.Log("ouch: %q doesn't start with %q", completionItem, fragment)
+					continue
+				}
+				prediction = append(prediction, completionItem+"/", completionItem+"/...")
+			}
+		}
 	}
-	return complete.PredictFilesSet(listPackages(prediction[0])).Predict(a)
+	return
+}
+
+func findGopath() []string {
+	gopath := os.Getenv("GOPATH")
+	// complete.Log("gopath = %v", gopath)
+	if gopath == "" {
+		// By convention
+		usr, err := user.Current()
+		if err != nil {
+			return nil
+		}
+		usrgo := filepath.Join(usr.HomeDir, "go")
+		return []string{usrgo}
+	}
+	listsep := string([]byte{os.PathListSeparator})
+	entries := strings.Split(gopath, listsep)
+	return entries
 }
 
 // listPackages looks in current pointed dir and in all it's direct sub-packages
@@ -39,7 +91,7 @@ func listPackages(dir string) (directories []string) {
 
 	// import packages according to given paths
 	for _, p := range paths {
-		pkg, err := build.ImportDir(p, 0)
+		pkg, err := build.Import("/home/valentin/go/src", p, 0)
 		if err != nil {
 			complete.Log("failed importing directory %s: %s", p, err)
 			continue
